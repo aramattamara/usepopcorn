0b9897a5dd39e2b835173a7324346fc5b27c769c
Tamara <lazerkatamara@gmail.com>
2023-08-17 14:22:04 -0700

    Replace http to https

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ export default function App() {
           try {
             setIsLoading(true);
             setError("");
-            const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {signal: controller.signal});
+            const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {signal: controller.signal});
 
             if(!res.ok) throw new Error("Something went wrong when fetching movies");
 
@@ -313,7 +313,7 @@ function MovieDetails({selectedId, onCloseMovie, onAddWatched, watchedMovies}){
   useEffect(function(){
     async function getMovieDetails (){
       setIsLoading(true)
-      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`);
+      const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`);
       const data = await res.json()
       setMovie(data)
       setIsLoading(false)
